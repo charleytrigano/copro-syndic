fdabeafc4c7658b0eb6322cd3e49786b7cdaa697
charleytrigano <charley@trigano.org>
2026-02-11 09:43:48 +0100

    Update pdf_utils.py

--- pdf_utils.py
+++ pdf_utils.py
@@ -1,1 +1,11 @@
+# pdf_utils.py
+from weasyprint import HTML
+
+def generer_pdf_releve(copro, lignes):
+    html = f"<h1>Relevé de compte {copro.nom}</h1>"
+    for l in lignes:
+        html += f"<p>{l.montant_du} € - {l.montant_paye} €</p>"
+    fichier = f"assets/releve_{copro.id}.pdf"
+    HTML(string=html).write_pdf(fichier)
+    return fichier
 
